eadc1e2b58bad4d38ab60b68b7322523efd90eb3
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-01 16:27:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,11 +17,10 @@ function whileLoop(n){
 }
 
 function doWhileLoop(array){
-   function maybeTrue(){
+  do {
+     function maybeTrue(){
       return Math.random() >= 0.5;
     }
-    
-  do {
     array.pop();
   } while (array.length > 0 || maybeTrue());
 }
